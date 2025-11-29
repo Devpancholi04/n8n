8daa5d011c7733b1ee60fe5079bbbce129364114
Dev Pancholi <112951112+Devpancholi04@users.noreply.github.com>
2025-11-29 15:27:08 +0530

    made few changes with the passing the CA PEM secerts with the render

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Create secrets directory
 mkdir -p /etc/secrets
-echo $AIVEN_CA_PEM_B64 | base64 -d > /etc/secrets/ca.pem
-exec docker-entrypoint.sh "$@"
+
+# Decode Aiven CA cert from Base64 → ca.pem
+echo "$AIVEN_CA_PEM_B64" | base64 -d > /etc/secrets/ca.pem
+
+# Start n8n normally
+exec n8n
